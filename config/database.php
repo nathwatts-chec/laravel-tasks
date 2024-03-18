7993845b2f962b07d21cd99af3c5eb618c3ad08f
nathwatts-chec <162463680+nathwatts-chec@users.noreply.github.com>
2024-03-18 14:12:56 +0000

    Enable TLS

--- config/database.php
+++ config/database.php
@@ -132,6 +132,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
+            'scheme' => 'tls',
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '0'),
@@ -140,6 +141,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'scheme' => 'tls',
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
